ccc0a9095132d8d658946d9278b7acb822e88ea3
Duc Anh Trinh <ducanh.trinh@hotmail.de>
2018-05-02 11:12:10 +0200

    broadcasting dummy directions

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const socketIo = require('socket.io');
-const SerialPort = require('serialport');
+// const SerialPort = require('serialport');
 const { loggerStderr, loggerStdoutNl } = require(`${__dirname}/src/utilities`);
 const socketIoHandler = require(`${__dirname}/src/socketIoHandler`);
-const spHandler = require(`${__dirname}/src/spHandler`);
+// const spHandler = require(`${__dirname}/src/spHandler`);
 
 
 app.use(express.static('gui'));
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
     res.sendFile(`${__dirname}/gui/index.html`);
 });
 
-const server = app.listen(1337, () => {
+const server = app.listen(3000, () => {
     loggerStdoutNl(server.address());
 });
 
@@ -23,17 +23,24 @@ const io = socketIo(server);
 io.on('connect', (socket) => {
     socket.on('message', (message) => {
         socketIoHandler.messageHandler(message);
+        socket.broadcast.emit('message', JSON.stringify({
+            action: 'BOBBLE_DIRECTION',
+            payload: { 
+                x: Math.random()*100,
+                y: Math.random()*100
+            }
+        }));
     });
 
     socket.on('disconnect', loggerStdoutNl);
 });
 
 
-SerialPort
-    .list()
-    .then((ports) => ports.map(spHandler.portHandler))
-    .then((parsers) => parsers.forEach((parser) => {
-        parser.on('data', spHandler.dataHandler);
-        parser.on('error', loggerStderr);
-    }))
-    .catch(loggerStderr);
+// SerialPort
+//     .list()
+//     .then((ports) => ports.map(spHandler.portHandler))
+//     .then((parsers) => parsers.forEach((parser) => {
+//         parser.on('data', spHandler.dataHandler);
+//         parser.on('error', loggerStderr);
+//     }))
+//     .catch(loggerStderr);
